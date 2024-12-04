6cf99a5acdc1b1cedfb241d950c8e56702a1e347
FerranAgulloLopez <ferranagullolopez@gmail.com>
2024-12-04 20:30:25 +0100

    upd: uploads initial results of offline profiler

---
+++ benchmarks_simple/definitive_results/initial/llama-2-7b/_250_230_64/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=_250_230_64
+#SBATCH -D ./
+#SBATCH --ntasks=1
+#SBATCH --output=benchmarks_simple/definitive_results/initial/llama-2-7b/_250_230_64/log_%j.out
+#SBATCH --error=benchmarks_simple/definitive_results/initial/llama-2-7b/_250_230_64/log_%j.err
+#SBATCH --cpus-per-task=20
+#SBATCH --gres gpu:1
+#SBATCH --time=00:10:00
+module load singularity
+singularity exec --nv --env PYTHONPATH=/usr/local/lib/python3.12/dist-packages /gpfs/scratch/bsc98/bsc098069/llm_benchmarking/images/vllm-benchmark-default-nsight.sif python3 benchmarks_simple/offline_profiler.py --result-dir='benchmarks_simple/definitive_results/initial/llama-2-7b/_250_230_64' --model=/gpfs/scratch/bsc98/bsc098069/llm_benchmarking/models/llama/llama-2-7b --json=times --save-chrome-traces-folder=chrome_traces --max-num-batched-tokens=256000 --max-num-seqs=1024 --prompt-len='250' --output-len='230' --batch-size='64'
